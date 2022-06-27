45d0c586c1f4e013739347e275362ab7787e9246
JMLemos <jlemosdecastro@gmail.com>
2022-06-27 16:51:42 -0300

    Add class Piano

---
+++ ConsoleApp2/Piano.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Piano
+    { 
+
+        public string Type { get; set; }
+        public int NumKeys { get; set; }
+        public bool IsEletric { get; set; }
+
+    }
+}
